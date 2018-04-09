a196c9062a7a61b500ba7000e42ee6c253ccdde3
siuming818 <siuming818@gmail.com>
2018-04-09 22:57:35 +0800

    Updated restaurant list

--- app/Http/Controllers/AttractionController.php
+++ app/Http/Controllers/AttractionController.php
@@ -118,7 +118,7 @@ class AttractionController extends Controller
             });
             if (!$city) throw new NotFoundHttpException(trnas('notfound.city'));
             $attractions = Cache::remember("attractions_restaurants_city_{$id}", 10, function() use ($city, $id){
-                $tags = array("4bf58dd8d48988d1c4941735", "52e81612bcbc57f1066b79f8", "4bf58dd8d48988d1c7941735", "58daa1558bbb0b01f18ec1d3", "4bf58dd8d48988d16d941735", "4bf58dd8d48988d1e0931735");
+                $tags = array("4bf58dd8d48988d1c4941735", "52e81612bcbc57f1066b79f8", "4bf58dd8d48988d1c7941735", "58daa1558bbb0b01f18ec1d3", "4bf58dd8d48988d16d941735", "4bf58dd8d48988d1e0931735", "52af3b773cf9994f4e043c03", "55a59bace4b013909087cb30", "52af3add3cf9994f4e043bf5", "52af3b6e3cf9994f4e043c02", "4bf58dd8d48988d1cb941735", "4bf58dd8d48988d1ce941735", "4bf58dd8d48988d10f941735", "52af0bd33cf9994f4e043bdd", "4bf58dd8d48988d1d2941735", "4bf58dd8d48988d1d3941735", "4bf58dd8d48988d1d1941735", "4bf58dd8d48988d111941735");
                 $condition = implode(" OR ", array_map(function($tag) {
                     return "JSON_CONTAINS(tags, '\"$tag\"')";
                 }, $tags));
@@ -139,23 +139,30 @@ class AttractionController extends Controller
 
     public function getAttractions(Request $request, $id)
     {
+        $currentPage = $request->page ?? 0;
+        $perPage = $request->limit ?? 10;
+
         try {
             $city = Cache::remember("city_$id", 10, function () use ($id) {
                 return City::find($id);
             });
             if (!$city) throw new NotFoundHttpException(trnas('notfound.city'));
             $attractions = Cache::remember("attractions_attractions_city_{$id}", 10, function() use ($city, $id){
-                $tags = array("4bf58dd8d48988d1fa931735", "4bf58dd8d48988d1ee931735", "4bf58dd8d48988d1fb931735", "4bf58dd8d48988d12f951735", "4bf58dd8d48988d1fa931735", "52e81612bcbc57f1066b79f8", "4bf58dd8d48988d1c7941735", "58daa1558bbb0b01f18ec1d3", "4bf58dd8d48988d16d941735", "5109983191d435c0d71c2bb1", "4bf58dd8d48988d1e0931735");
+                $tags = array("4bf58dd8d48988d1fa931735", "4bf58dd8d48988d1ee931735", "4bf58dd8d48988d1fb931735", "4bf58dd8d48988d12f951735", "4bf58dd8d48988d1c4941735", "52e81612bcbc57f1066b79f8", "4bf58dd8d48988d1c7941735", "58daa1558bbb0b01f18ec1d3", "4bf58dd8d48988d16d941735", "5109983191d435c0d71c2bb1", "4bf58dd8d48988d1e0931735", "4bf58dd8d48988d132951735", "52af3b773cf9994f4e043c03", "55a59bace4b013909087cb30", "52af3add3cf9994f4e043bf5", "52af3b6e3cf9994f4e043c02", "4bf58dd8d48988d1cb941735", "4bf58dd8d48988d1ce941735", "4bf58dd8d48988d10f941735", "52af0bd33cf9994f4e043bdd", "4bf58dd8d48988d1d2941735", "4bf58dd8d48988d1d3941735", "4bf58dd8d48988d1d1941735", "4bf58dd8d48988d111941735");
                 $condition = implode(" AND ", array_map(function($tag) {
                     return "NOT JSON_CONTAINS(tags, '\"$tag\"')";
                 }, $tags));
                  return $city->attractions()->whereRaw("(".$condition.") AND city_id = ".$id)->get();
              });
 
+             $collection = new Collection($attractions);
+             $currentPageBlogResults = $collection->slice($currentPage * $perPage, $perPage)->all();
+             $paginatedBlogResults= new LengthAwarePaginator($currentPageBlogResults, count($collection), $perPage);
+
         } catch (\PDOException $e) {
             throw new ServiceUnavailableHttpException('', trans('custom.unavailable'));
         }
-        return $this->response->item($attractions, new AttractionTransformer, ['key' => 'data']);
+        return $this->response->paginator($paginatedBlogResults, new AttractionTransformer, ['key' => 'attractions']);
     }
 
 
